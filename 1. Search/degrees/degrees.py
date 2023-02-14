c9620eb63ed4dd4d71866e7a185ba00458177302
Diego Arnoldo Azuela Rosas <diego.azuela@gmail.com>
2023-02-14 11:20:37 -0600

    <build>setting foundations to complete task

--- 1. Search/degrees/degrees.py
+++ 1. Search/degrees/degrees.py
@@ -57,7 +57,7 @@ def main():
     t0 = time.time()
     if len(sys.argv) > 2:
         sys.exit("Usage: python degrees.py [directory]")
-    directory = sys.argv[1] if len(sys.argv) == 2 else "small"
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
 
     # Load data from files into memory
     print("Loading data...")
